edb5e7b4bba49f2c85ad3d6e2f21f9b9356cfda2
sebaas24 <158789286+sebaas24@users.noreply.github.com>
2025-09-21 20:01:16 -0600

    Delete Desktop/U Galileo/TECNICO EN DESARROLLO DE SOFTWARE/5to. Trimestre/Programación Avanzada/Actividad2/Fronted directory

--- Desktop/U Galileo/TECNICO EN DESARROLLO DE SOFTWARE/5to. Trimestre/Programación Avanzada/Actividad2/Fronted/my-app/features/habit/habitAPI.ts
+++
@@ -1,7 +0,0 @@
-export const fetchHabits = async () => {
-  const response = await fetch("https://localhost:3000/habits");
-  if (!response.ok) {
-    throw new Error("Failed to fetch habits");
-  }
-  return response;
-}
